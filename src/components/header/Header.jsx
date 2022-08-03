8c5e60dc97763918a76fb7219b846a452a707936
Praguu <101408147+Praguu@users.noreply.github.com>
2022-08-03 09:40:54 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,40 @@ import './header.css';
 import CTA from './CTA';
 import ME from '../../assests/ME.jpg';
 import HeaderSocials from './HeaderSocials';
+import Typical from "react-typical";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello i am</h5>
-        <h1>pragya sharma</h1>
-        <h5 className='text-light'>Mern stack devloper</h5>
+      <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I am <span className="highlighted-text">Pragya</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Java Developer 😎",
+                    1000,
+                    "Frontend Developer 👩‍🎨",
+                    1000,
+                    "React Beginner 👩‍🏭",
+                    1000 /*  */,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Knack of building application with front and back end
+                operations.
+              </span>
+            </span>
+          </div>
         <CTA />
         <HeaderSocials /> 
       
